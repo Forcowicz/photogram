18f97ec0fbd415fb45a0b3bae10d338f1912baaf
forcowicz <forcowicz@gmail.com>
2022-12-05 01:33:30 +0100

    User resource basics

--- models/userModel.js
+++ models/userModel.js
@@ -12,6 +12,22 @@ const userSchema = new mongoose.Schema({
     unique: [true, "This email has been already taken!"],
     lowercase: true,
   },
+  password: {
+    type: String,
+    required: [true, "Please provide your desired password!"],
+    minlength: [8, "Password must be at least 8 characters long"],
+    maxlength: [32, "Password can be 32 characters long"],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please re-enter your password!"],
+    validate: {
+      validator: function (val) {
+        return this.password === val;
+      },
+      message: "Passwords do not match!",
+    },
+  },
 });
 
 const User = mongoose.model("User", userSchema);
